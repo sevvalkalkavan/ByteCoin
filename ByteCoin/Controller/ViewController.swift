d5827415ef36df95c2521aff11490e0ba38f9a66
Sevval Kalkavan <sevvallkalkavannn@gmail.com>
2023-06-16 15:29:32 +0300

    UIPickerView Functionality,Using UIPicker, we made the selected value show on the screen.

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    var coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+        
     }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency : String
+        selectedCurrency = coinManager.currencyArray[row]
+        currencyLabel.text = selectedCurrency
+        coinManager.getCoinPrice(for: selectedCurrency)
+        print(selectedCurrency)
+    }
 
 }
 
